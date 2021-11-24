f43991bfbbdd92b4dd98cefec9971fb695ee687d
Luiz Lelis <luizhlelis@gmail.com>
2021-11-24 19:28:19 -0300

    chore: default open id sample

---
+++ src/PingIdentityDotnet/Startup.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols;
+
+namespace PingIdentityDotnet
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddApiVersioning(options => {
+                options.ReportApiVersions = true;
+
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1,0);
+            });
+
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            var openApiInfo = new OpenApiInfo();
+            Configuration.Bind("OpenApiInfo", openApiInfo);
+            services.AddSingleton(openApiInfo);
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+            services.AddSwaggerGen(options =>
+            {
+                //options.SwaggerDoc("v1", openApiInfo);
+                options.OperationFilter<SwaggerDefaultValues>();
+            });
+
+            // Cookie configuration for HTTP to support cookies with SameSite=None
+            services.ConfigureSameSiteNoneCookies();
+
+            // Add authentication services
+            //var issuer = "https://localhost:9031";
+            //var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+            //    issuer + "/.well-known/oauth-authorization-server",
+            //    new OpenIdConnectConfigurationRetriever(),
+            //    new HttpDocumentRetriever()
+            //);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect("PingIdentity", options => {
+                // Set the authority to your PingIdentity domain
+                options.Authority = $"https://{Configuration["AuthorizationServer:Domain"]}";
+
+                // Configure the PingIdentity Client ID and Client Secret
+                options.ClientId = Configuration["AuthorizationServer:ClientId"];
+                options.ClientSecret = Configuration["AuthorizationServer:ClientSecret"];
+
+                // Set response type to code
+                options.ResponseType = OpenIdConnectResponseType.Code;
+
+                // Configure the scope
+                options.Scope.Clear();
+                var scopeArray = Configuration["AuthorizationServer:Scopes"].Split(',');
+                foreach (var scope in scopeArray)
+                    options.Scope.Add(scope);
+
+                // Also ensure that you have added the URL as an Allowed Callback URL in your PingIdentity dashboard
+                options.CallbackPath = new PathString("/v1/auth/response-oidc");
+
+                // Configure the Claims Issuer to be PingIdentity
+                options.ClaimsIssuer = "PingIdentity";
+
+                options.SaveTokens = true;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "name"
+                };
+
+
+                options.Events = new OpenIdConnectEvents
+                {
+                    OnRedirectToIdentityProviderForSignOut = (context) =>
+                    {
+                        var logoutUri = $"{options.Authority}/v2/logout?client_id={options.ClientId}";
+
+                        var postLogoutUri = context.Properties.RedirectUri;
+                        if (!string.IsNullOrEmpty(postLogoutUri))
+                        {
+                            if (postLogoutUri.StartsWith("/"))
+                            {
+                                var request = context.Request;
+                                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                            }
+                            logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                        }
+
+                        context.Response.Redirect(logoutUri);
+                        context.HandleResponse();
+
+                        return Task.CompletedTask;
+                    },
+
+                    OnAuthorizationCodeReceived = (context) =>
+                    {
+                        Console.WriteLine(context.JwtSecurityToken);
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            // Add framework services.
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+
+            app.UseRouting();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseSwaggerUI(c =>
+                {
+                    foreach(var description in provider.ApiVersionDescriptions)
+                        c.SwaggerEndpoint(
+                            $"/swagger/{description.GroupName}/swagger.json",
+                            description.GroupName);
+                }
+            );
+
+            app.UseCors(builder => builder.AllowAnyMethod()
+                                          .AllowAnyOrigin()
+                                          .AllowAnyHeader());
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ src/PingIdentityDotnet/ViewModel/UserProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PingIdentityDotnet.ViewModel
+{
+    public class UserProfileViewModel
+    {
+        public string EmailAddress { get; set; }
+
+        public string Name { get; set; }
+
+        public string ProfileImage { get; set; }
+    }
+}
